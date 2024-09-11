68f0e802cd6636610c6e57d9ecdd6bc45c5d2d71
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-09-11 11:04:26 +0600

    all problems done

--- problem3.js
+++ problem3.js
@@ -1,20 +1,20 @@
 function checkDigitsInName(name){
-let named = name.toString();
 
-if(typeof named !== "string"){
+if(typeof name !== "string"){
     return "Invalid Input"
 }
+
     for(const num of name){
-        if(typeof isNaN(num[0]) !== 'number'){
-            return true
-        }else{
-            return false
-        }
+       if (!isNaN(num)){
+        return true
+       }
     }
+    return false
+    
 }
 // test
-console.log(checkDigitsInName("Raj"))
-// console.log(checkDigitsInName("Sumon"))
-// console.log(checkDigitsInName("Name2024"))
-// console.log(checkDigitsInName("!@#"))
-// console.log(checkDigitsInName(["Raj"]))
+console.log(checkDigitsInName("Raj123"))
+console.log(checkDigitsInName("Sumon"))
+console.log(checkDigitsInName("Name2024"))
+console.log(checkDigitsInName("!@#"))
+console.log(checkDigitsInName(["Raj"]))
